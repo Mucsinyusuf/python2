fafc8793777e10386b25e88d5d8c814d8868a488
mucsinyusuf <muxsinyussuf@gmail.com>
2024-12-15 12:09:17 +0300

    day1

---
+++ Day1.py
@@ -0,0 +1,58 @@
+# printing in python 
+print("My name is mucsin !")
+print ("mucsin", end="-")
+print ("Yusuf")
+
+#Data types
+
+# Integer 
+# is a number without any deciml point 
+# forexample 
+print (f"3" + "is an integer  ")
+
+# Float 
+# Numbers with decimals 
+# forexample 
+print (f"3.14" + "Is a float ")
+
+
+
+# Boolean
+# is a true or false 
+#forexample 
+print(True)
+
+
+
+# String
+# Is a text 
+# forexample 
+print("this is a string type")
+
+
+
+# Complex
+#Is a complex type data ie a complex number
+# forexample 
+print (type(5+9j))
+
+# List = []
+# a list of values 
+print (type([1,2,3,4]))
+
+
+
+# Tuple = ()
+print (type((1,2,3,4,5)))
+
+# Sets = {}
+print (type({1,2,3,4}))
+
+
+# Dictionary = {}
+print({"Name":"moha", "age":23 ,"Gender":"male"})
+print (type({"Name":"moha", "age":23 ,"Gender":"male"}))
+
+
+
+
