ec8f425981590e0393dc9718afe9e9381be6fa44
mucsinyusuf <muxsinyussuf@gmail.com>
2024-12-17 14:31:46 +0300

    Day2

--- Day2.py
+++ Day2.py
@@ -1,44 +1,44 @@
-# # if/else conditional statement
-# email = input("Enter your email")
-# password = input("Enter password")
+# if/else conditional statement
+email = input("Enter your email")
+password = input("Enter password")
 
-# if email == 'mucsin@gmail.com' and password == 'Alhamdulillah' :
-#     print("Login Successfull")
-# elif email == 'mucsin@gmail.com' or password == 'Alhamdulillah':
-#     print("try with another credentials")
-# else:
-#     print("Wrong password")
+if email == 'mucsin@gmail.com' and password == 'Alhamdulillah' :
+    print("Login Successfull")
+elif email == 'mucsin@gmail.com' or password == 'Alhamdulillah':
+    print("try with another credentials")
+else:
+    print("Wrong password")
 
 
-# # nested if/else condition
-# if email == 'mucsin@gmail.com' and password == 'Alhamdulillah' :
-#     print("Login Successfull")
-# elif email == 'mucsin@gmail.com' or password != 'Alhamdulillah':
-#     print("try with another credentials")
-#     password = input("Enter password again")
-#     if password :
-#         print("Success")
-#     else:
-#         print("Wrong")
-# else:
-#     print("Wrong password")
+# nested if/else condition
+if email == 'mucsin@gmail.com' and password == 'Alhamdulillah' :
+    print("Login Successfull")
+elif email == 'mucsin@gmail.com' or password != 'Alhamdulillah':
+    print("try with another credentials")
+    password = input("Enter password again")
+    if password :
+        print("Success")
+    else:
+        print("Wrong")
+else:
+    print("Wrong password")
 
 
 
 # Excersice
 # find the min of three numbers
-# num1 = int(input("Enter first number ")) 
-# num2 = int(input("Enter second number "))
-# num3 = int(input("Enter third number "))
-# if num1 < num2 and num1 < num3 :
-#     print(f"{num1}" + " " "is the min")
-# elif num2 < num1 and num2 < num3 :
-#     print(f"{num2}" + " " "is the min")
-# elif num3 < num1 and num3 < num2:
-#     print(f"{num3}" + " " "is the min")
+num1 = int(input("Enter first number ")) 
+num2 = int(input("Enter second number "))
+num3 = int(input("Enter third number "))
+if num1 < num2 and num1 < num3 :
+    print(f"{num1}" + " " "is the min")
+elif num2 < num1 and num2 < num3 :
+    print(f"{num2}" + " " "is the min")
+elif num3 < num1 and num3 < num2:
+    print(f"{num3}" + " " "is the min")
 
 # excersice 2
-#ATM Machine menu
+# ATM Machine menu
 
 menu =  input("""
 hi there welconme 
