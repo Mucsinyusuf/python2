0a7d4175b5a118b0716eff7fb8e8e2ac578d6ba4
mucsinyusuf <muxsinyussuf@gmail.com>
2024-12-19 10:59:35 +0300

    Daqy3

---
+++ Day3.py
@@ -0,0 +1,14 @@
+# Modules in python
+# is a file inside python that a programmers has already written for u 
+import math
+import datetime
+x = math.sqrt(196)
+print(x)
+
+z = math.factorial(4)
+print(z)
+
+y = datetime.datetime.now()
+print(y)
+
+# pip in python
